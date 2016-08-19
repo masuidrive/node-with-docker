041e636c1cc8672815e18ec7916661a21e26ee0f
Yuichiro MASUI <masui@masuidrive.jp>
2016-08-19 19:30:55 +0900

    Init

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM node:4.4.7
 WORKDIR /usr/src/app
 RUN mkdir -p /usr/src/app
 ADD package.json /usr/src/app/package.json
-RUN npm install -g
+RUN npm install --global
 
 COPY . /usr/src/app
+EXPOSE 3000
 CMD npm run server
--- hello_app.js
+++ hello_app.js
@@ -1,13 +1,8 @@
-var redis = require('redis');
-var client = redis.createClient(process.env.REDIS_HOST, process.env.REDIS_PORT);
-
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  client.get('counter', function (error, buffer) {
-    res.send('Hello World!');
-  });
+  res.send('Hello World!');
 });
 
 app.listen(3000, function () {
